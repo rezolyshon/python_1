36ec0e4d0bcfccda3e84de70c78c578e6f65400b
rezolyshon <100044088+rezolyshon@users.noreply.github.com>
2023-07-29 21:46:07 +0300

    start commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# def min_flips(coins):
+#  heads = sum(coins)
+#  tails = len(coins) - heads
+#  return min(heads, tails)
+
+# coins = [1, 0, 1, 1, 0, 0, 1]  
+# print(min_flips(coins)) 
+
+
+# import random
+# s = int(input('Задай сумму двух чисел \n'))
+# p = int(input('Задай произведение чисел \n'))
+# for x in range(s):
+#     for y in range(p):
+#         if s == x + y and p == x * y:
+#             print(f'первое число ="{x}", второе число ="{y}"')
+
+
+# n = int(input('Введи число N:'))
+# i = 0
+# while 2 ** i <= n:
+#     print(f' 2 в степени {i} равна {2 ** i}')
+#     i += 1
